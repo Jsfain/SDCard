2183c16668ade69e7461c72b05b4aed257864736
Joshua Fain <joshua.s.fain@gmail.com>
2020-09-18 13:31:08 -0400

    removed SD_MSG string literals from sd_SPI_Mode_Init() and updated some variable names

--- Modules/Disk/SD_Card/source/sd_spi_base.c
+++ Modules/Disk/SD_Card/source/sd_spi_base.c
@@ -5,7 +5,7 @@
  * ATmega 1280
  *
  * DESCRIPTION
- * Defines the base-level SD Card functions declared in SD_SPI.H
+ * Defines the base-level SD card functions for SPI mode.
  * 
  * FUNCTION LIST
  * 1) uint32_t  sd_SPI_Mode_Init(void)
@@ -22,6 +22,7 @@
  * ***************************************************************************/
  
 
+
 #include <stdint.h>
 #include <avr/io.h>
 #include "../includes/sd_spi_base.h"
@@ -33,181 +34,91 @@
 // Initializes a standard capacity SD card into SPI mode
 uint32_t sd_SPI_Mode_Init(void)
 {
-    if(SD_MSG > 1) print_str("\n\n\r>> INFO:    ***** BEGIN sd_SPI_Mode_Init()");
-    
-    //Initialize response variables
-    uint8_t R1 = 0; //R1 response returned for every SD Command 
+    uint8_t R1 = 0;//R1 response returned for every SD Command 
     uint8_t R7[5]; //R7 response returned by SEND_IF_COND (CMD8)
 
-    for(int i=0;i<=10;i++)  sd_SendByte(0xFF); //Wait 80 clock cycles for power up to complete.
+    //Wait up to 80 clock cycles for power up to complete.
+    for(int i=0;i<=10;i++) { sd_SendByte(0xFF); }
 
     // ********************
-    // GO_IDLE_STATE (CMD0) : place card in SPI mode  
-    CS_LOW; // Assert CS          
-    sd_SendCommand(GO_IDLE_STATE, 0);  //CMD0;
-    R1 = sd_getR1(); // Get R1 response
-    CS_HIGH; // Deassert CS
-    if(SD_MSG > 2) {print_str("\n\r>> DEBUG:   Printing R1 response returned from sd_getR1() for command GO_IDLE_STATE (CMD0):"); sd_printR1(R1);}
-    if (R1 != 1)
-    {
-        if(SD_MSG) 
-        {
-            print_str("\n\r>> ERROR:   GO_IDLE_STATE (CMD0) in sd_SPI_Mode_Init() returned error in R1 response.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
-        return (FAILED_GO_IDLE_STATE | R1);
-    }
-    //END GO_IDLE_STATE (CMD0)
+    // GO_IDLE_STATE (CMD0)
+    CS_LOW;       
+    sd_SendCommand(GO_IDLE_STATE, 0);
+    R1 = sd_getR1();
+    CS_HIGH;
+    if (R1 != 1) { return (FAILED_GO_IDLE_STATE | R1); }
+    // END GO_IDLE_STATE (CMD0)
     // *******************
 
 
 
     // *******************
-    // SEND_IF_COND (CMD8): get SD Interface Conditions. Confirm card type is valid/supported.
-    uint8_t cp = 0xAA; //check pattern
-    uint8_t v  = 0x01; //supply voltage: range = 2.7 to 3.6V
+    // SEND_IF_COND (CMD8)
+    uint8_t checkPattern = 0xAA;
+    uint8_t voltageSupplyRange  = 0x01; // 2.7 to 3.6V
     
     CS_LOW;
-    sd_SendCommand(SEND_IF_COND, ((uint16_t)v<<8)|cp); //CMD8
+    sd_SendCommand(SEND_IF_COND,
+                  ((uint16_t)voltageSupplyRange << 8 ) | checkPattern );
 
     //Get R7 response
-    R7[0] = sd_getR1(); // First byte of R7 response is the R1 response.
+    R7[0] = sd_getR1(); // First R7 byte is R1 response.
     R7[1] = sd_ReturnByte();
     R7[2] = sd_ReturnByte();
     R7[3] = sd_ReturnByte();
     R7[4] = sd_ReturnByte();
     
     CS_HIGH;
+    if (R7[0] != 1) { return (FAILED_SEND_IF_COND | R7[0]); }
 
-    if(SD_MSG > 2) {print_str("\n\r>> DEBUG:   Printing R1 response returned from sd_getR1() for command SEND_IF_COND (CMD8):"); sd_printR1(R7[0]);}
-    if (R7[0] != 1) //R7[0] is R1 response returned for CMD8
-    {
-        if(SD_MSG) 
-        {
-            print_str("\n\r>> ERROR:   SEND_IF_COND (CMD8) in sd_SPI_Mode_Init() returned error in R1 response.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
-        return (FAILED_SEND_IF_COND | R7[0]);
-    }
-
-    if(R7[3]!=0x01) //Check the rest of the response to confirm if SEND_IF_COND is valid.
-    {
-        if(SD_MSG)
-        {
-            print_str("\n\r>> ERROR:   Voltage mismatch detected in SD Card's response to SEND_IF_COND (CMD8) in sd_SPI_Mode_Init().");
-            print_str("\n\r            Either SD card version is invalid/unsupported OR the 'supply voltage' portion of the argument in SEND_IF_COND command is incorrect. ");
-            print_str("\n\r            Ensure 'supply voltage' argument = 0x01.");
-            print_str("\n\r            Ensure SD Card is version 2.0 or later.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
-        return (FAILED_SEND_IF_COND | UNSUPPORTED_CARD_TYPE | R7[0]);
-    }
-    if(R7[4]!=cp)
-    {
-        if(SD_MSG)
-        {
-            print_str("\n\r>> ERROR:   Check Pattern failed in SD Card's response to SEND_IF_COND (CMD8) in sd_SPI_Mode_Init().");
-            print_str("\n\r            Check pattern sent with SEND_IF_COND command to SD Card:");
-            print_str(" 0x");print_hex(cp);
-            print_str("\n\r            Check pattern returned by SD Card:");
-            print_str(" 0x");print_hex(R7[4]);
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
-        return (FAILED_SEND_IF_COND | UNSUPPORTED_CARD_TYPE | R7[0]);
-    }
-    //END SEND_IF_COND (CMD8)
+    if ( (R7[3] != voltageSupplyRange ) || (R7[4] != checkPattern) )
+    { return (FAILED_SEND_IF_COND | UNSUPPORTED_CARD_TYPE | R7[0]); }
+    // END SEND_IF_COND (CMD8)
     // **********************
 
 
 
     // **********************
-    // CRC_ON_OFF (CMD59): turns CRC ON or OFF. Should be off by default, 
-    R1 = 0; // Reset R1
+    // CRC_ON_OFF (CMD59)
+    R1 = 0;
     CS_LOW;
-    sd_SendCommand(CRC_ON_OFF,0);  //Send CMD 59. 
-                                   //arg = 0 CRC OFF (default for SPI mode). 
-                                   //arg = 1 CRC ON 
-    R1 = sd_getR1(); // Get R1 response
+    sd_SendCommand(CRC_ON_OFF,0);  //arg = 0 CRC OFF (default) 
+                                   //arg = 1 CRC ON
+    R1 = sd_getR1();
     CS_HIGH;
-    if(SD_MSG > 2) {print_str("\n\r>> DEBUG:   Printing R1 response returned from sd_getR1() for command CRC_ON_OFF (CMD59):"); sd_printR1(R1);}
-    if (R1 != 1)
-    {
-        if(SD_MSG) 
-        {
-            print_str("\n\r>> ERROR:   CRC_ON_OFF (CMD59) in sd_SPI_Mode_Init() returned error in R1 response.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
-        return (FAILED_CRC_ON_OFF | R1);
-    }
-    //END CRC_ON_OFF (CMD59)
+    if (R1 != 1) { return (FAILED_CRC_ON_OFF | R1); }
+    // END CRC_ON_OFF (CMD59)
     // ***********************
 
 
 
     // ************************
-    // SD_SEND_OP_COND (ACMD41): 
-    // Send host capacity information to SD card. Continue sending until SD card signals it is no 
-    // longer in the idle state (R1 != 1). SD_SEND_OP_COND is an application command therefore each
-    // time it is sent it must be preceded by APP_CMD (CMD55) to signal the next command is ACMD type.
-    uint32_t acmd41_arg = 0;
-    if(HCS) // acmd41_arg = 0x40000000 if HCS supported;
-    {
-        if(SD_MSG)
-        {
-            print_str("\n\r>> ERROR:   HCS flag is > 0 indicating host should support SDHC/SDXC cards,"); 
-            print_str("\n\r            SDHC/SDXC cards are currently not supported by this host.");
-            print_str("\n\r            Set HCS flag to 0 and recompile."); 
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
-        return (UNSUPPORTED_CARD_TYPE | R1);
-    }
-
+    // SD_SEND_OP_COND (ACMD41)
+    uint32_t acmd41_arg = 0; // = 0x40000000 if HCS supported;
+    if(HCS) { return (UNSUPPORTED_CARD_TYPE | R1); } // HCS is not supported
     else
     {
-        uint8_t attempt = 0; //used for timeout returning OUT_OF_IDLE
+        // Continue sending host capacity info to SD card until card signals it
+        // is no longer in the idle state (R1 != 1) or times out.
+        uint8_t timeOut = 0;
         do{
-            R1 = 0; // Reset R1
+            R1 = 0;
             CS_LOW;
-            sd_SendCommand(APP_CMD,0); //send APP_CMD (CMD55) to signal next command is ACMD type.
+            sd_SendCommand(APP_CMD,0); //first send APP_CMD before any ACMD
             R1 = sd_getR1();
             CS_HIGH;
-            if(SD_MSG > 2) {print_str("\n\r>> DEBUG:   Printing R1 response returned from sd_getR1() for command APP_CMD (CMD55):"); sd_printR1(R1);}
-            if (R1 != 1)
-            {
-                if(SD_MSG)
-                {
-                    print_str("\n\r>> ERROR:   APP_CMD (CMD55) in sd_SPI_Mode_Init() returned error in R1 response.");
-                    print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-                }
-                return (FAILED_APP_CMD | R1);
-            }
-            
-            R1 = 0; // Reset R1
-            CS_LOW;
-            sd_SendCommand(SD_SEND_OP_COND,acmd41_arg); //send SD_SEND_OP_COND (ACMD41)
-            R1 = sd_getR1();
-            CS_HIGH;
-            if(SD_MSG > 2) {print_str("\n\r>> DEBUG:   Printing R1 response returned from sd_getR1() for command SD_SEND_OP_COND (ACMD41):"); sd_printR1(R1);}
-            if (R1 > 1)
-            {
-                if(SD_MSG) 
-                { 
-                    print_str("\n\r>> ERROR:   SD_SEND_OP_COND (ACMD41) in sd_SPI_Mode_Init() returned error in R1 response.");
-                    print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-                }
-                return (FAILED_SD_SEND_OP_COND | R1);
-            }
+            if (R1 != 1) { return (FAILED_APP_CMD | R1); }
 
-            if(attempt++ >= 0x05)
-            {
-                if(SD_MSG) 
-                {
-                    print_str("\n\r>> ERROR:   Time out while waiting for R1 response to SD_SEND_OP_COND to signal SD card is out of the idle state in sd_SPI_Mode_Init().");
-                    print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-                }
-                return (FAILED_SD_SEND_OP_COND | OUT_OF_IDLE_TIMEOUT | R1);
-            }
-        }while(R1&1);
+            R1 = 0;
+            CS_LOW;
+            sd_SendCommand(SD_SEND_OP_COND,acmd41_arg);
+            R1 = sd_getR1();
+            CS_HIGH;
+            if (R1 > 1) { return (FAILED_SD_SEND_OP_COND | R1); }
+
+            if(timeOut++ >= 0x05) 
+            { return (FAILED_SD_SEND_OP_COND | OUT_OF_IDLE_TIMEOUT | R1); }
+        }while( R1 & 1 );
     }
     // END SEND_OP_COND (ACMD41)
     // ************************
@@ -215,40 +126,27 @@ uint32_t sd_SPI_Mode_Init(void)
 
 
     // ************************
-    // READ_OCR (CMD58): 
-    // Final step for proper initialization is to read CCS (Card Capactity Status) bit
-    // of the OCR (Operating Condition Register). Currently only CCS = 0 is supported on host.
-    uint8_t resp;
-    R1 = 0; // Reset R1
+    // READ_OCR (CMD 58)
+
+    // Final initialization step is to read CCS (Card Capactity Status)
+    // bit of the OCR (Operating Condition Register).
+    R1 = 0;
     CS_LOW;
-    sd_SendCommand(READ_OCR,0);  //Send CMD 58
+    sd_SendCommand(READ_OCR,0);
     R1 = sd_getR1();
-    if(SD_MSG > 2) {print_str("\n\r>> DEBUG:   Printing R1 response returned from sd_getR1() for command READ_OCR (CMD58):"); sd_printR1(R1);}
-    if (R1 != 0)
-    {
-        if(SD_MSG)
-        {
-            print_str("\n\r>> ERROR:   READ_OCR (CMD58) in sd_SPI_Mode_Init() returned error in R1 response.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
-        return (FAILED_READ_OCR | R1);
-    }
+    if (R1 != 0) { return (FAILED_READ_OCR | R1); }
 
-    resp = sd_ReturnByte();  //Get the rest of the response to READ_OCR
+    uint8_t resp = sd_ReturnByte();  //Get the rest of the response to READ_OCR
 
-    //Check POWER_UP_STATUS
-    if((resp>>7)!=1)
+    //POWER_UP_STATUS
+    if( (resp >> 7) != 1 )
     {
         CS_HIGH;
-        if(SD_MSG)
-        {
-            print_str("\n\r>> ERROR:   POWER_UP_STATUS bit in OCR is 1. SD Card has not completed power up cycle.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
         return (POWER_UP_NOT_COMPLETE | R1);
     }
     
-    //Check rest of the OCR. Only CCS bit is needed to complete initialization of SD Card in SPI Mode.
+    // Confirm rest of OCR returned is valid, though, technically
+    // only CCS is needed to complete initialization.
     uint8_t CCS  = ((resp&0x40)>>6);
     uint8_t UHSII = ((resp&0x20)>>5);
     uint8_t CO2T = ((resp&0x10)>>3);
@@ -257,38 +155,26 @@ uint32_t sd_SPI_Mode_Init(void)
              VOLTAGE_RANGES_ACCEPTED <<= 1;
              VOLTAGE_RANGES_ACCEPTED |= (sd_ReturnByte()>>7);
 
-
-    if(CCS > 0)
+    if(CCS > 0) // currently only non-HCS supported.
     {
         CS_HIGH;
-        if(SD_MSG)
-        {
-            print_str("\n\r>> ERROR:   CCS bit of OCR is = 1 and is not currently supported by host.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
         return (FAILED_READ_OCR | UNSUPPORTED_CARD_TYPE | R1) ;
     }
 
     if( (UHSII !=  0) || //0 for non-UHSII
         (CO2T  !=  0) || //0 for Over 2TB NOT supported
         (S18A  !=  0) || //0 for NO switching to 1.8V Accepted.
-        (VOLTAGE_RANGES_ACCEPTED != 0x1FF)  )//Voltages over range 2.7 to 3.6V supported.
+        (VOLTAGE_RANGES_ACCEPTED != 0x1FF)  ) // Voltages over range 
+                                              // 2.7 to 3.6V supported.
     {
         CS_HIGH;
-        if(SD_MSG)
-        {
-            print_str("\n\r>> ERROR:   Unexpected response to READ_OCR in sd_SPI_Mode_Init() for UHSII, CO2T, S18A, or VOLTAGE_RANGES.");
-            print_str("\n\r>> ERROR:   Initialization FAILED. Returning from sd_SPI_Mode_Init() with error(s).");
-        }
         return (FAILED_READ_OCR | UNSUPPORTED_CARD_TYPE | R1);
     }
     CS_HIGH;
     // END READ_OCR (CMD58)
     // ************************
 
-    if(SD_MSG > 1) print_str("\n\r>> INFO:    ***** END sd_SPI_Mode_Init(): SD Card successfully initialized.");
     return OUT_OF_IDLE; //initialization succeded
-
 }
 //END sd_SPI_Mode_Init()
 
